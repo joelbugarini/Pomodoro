5eb5d06368bb8c754ae5abefffb99bf72f618dba
Joel Bugarini <joelbugarini@hotmail.com>
2015-04-23 11:32:07 -0700

    Change to single array for rests and work time

--- PomodoroWIndow.xaml.cs
+++ PomodoroWIndow.xaml.cs
@@ -18,66 +18,99 @@ namespace Pomodoro
 
         DispatcherTimer timer, seconds;
         //Rounds
-        int[] rest = { 5, 5, 5, 15 };
-        int WorkTime = 25;
+        int[] worker = { 25, 5, 25, 5, 25, 5, 25, 15 };
         int round = 0;
 
-        bool inWorkRound = true;
 
         TimeSpan time;
 
 
+        //Buttons
+        #region
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonPlay.Visibility = Visibility.Hidden;
+            ButtonPause.Visibility = Visibility.Visible;
+            ButtonPrevious.IsEnabled = true;
+            ButtonNext.IsEnabled = true;
+            if (timer == null) StartNewRound(); else StartRound();
+        }
+
+        private void Dragable_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void ButonWindow_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonNext_Click(object sender, RoutedEventArgs e)
+        {
+            NextRound();
+            timer.Stop();
+            seconds.Stop();
+            StartNewRound();
+        }
+
+        private void ButtonPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            PrevRound();
+            timer.Stop();
+            seconds.Stop();
+            StartNewRound();
+        }
+
+        private void ButtonPause_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            seconds.Stop();
+            ButtonPause.Visibility = Visibility.Hidden;
+            ButtonPlay.Visibility = Visibility.Visible;
+            ButtonNext.IsEnabled = false;
+            ButtonPrevious.IsEnabled = false;
+        }
+        #endregion
+
+
+        private TimeSpan UpdateTime(int Time)
+        {
+            time = new TimeSpan(0, Time, 0);
+            return time;
+        }
+        
         private void StartRound() 
         {
             timer.Start();
             seconds.Start();
         }
+
         private void StartNewRound()
         {
-            if (inWorkRound)
-            {
-                CreateWorkTimer();
-                CreateSmallHandTimer();                
-            }
-            else
-            {
-                CreateRestTimer();
-                CreateSmallHandTimer();
+            CreateWorkTimer();
+            CreateSmallHandTimer();
 
-            }
             timer.Start();
             seconds.Start();
 
             ButtonNext.IsEnabled = true;
             ButtonPrevious.IsEnabled = true;
-        }
-
-
-        private DispatcherTimer CreateRestTimer()
-        {
-            timer = new DispatcherTimer();//
-            timer.Interval = TimeSpan.FromMinutes(rest[round]);
-            timer.Tick += OnWorkTimeEnd;
-
-            time = new TimeSpan(0, rest[round],0);
-           
-            return timer;
-        }
-
-        private void NextRound()
-        {
-            if (inWorkRound)
-            {
-                if (round == rest.Length - 1) round= 0; else round++;
-            }
-        }
-
-        private void PrevRound()
-        {
-            if (inWorkRound)
-            {
-                if (round == 0) round = rest.Length - 1; else round--;
-            }
         }
 
         private DispatcherTimer CreateSmallHandTimer()
@@ -94,20 +127,20 @@ namespace Pomodoro
         private DispatcherTimer CreateWorkTimer()
         {
             timer = new DispatcherTimer();//
-            timer.Interval = TimeSpan.FromMinutes(WorkTime);
+            timer.Interval = UpdateTime(worker[round]);
             timer.Tick += OnWorkTimeEnd;
 
-            time = new TimeSpan(0, WorkTime, 0);
             return timer;
         }
 
-        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        private void NextRound()
         {
-            ButtonPlay.Visibility = Visibility.Hidden;
-            ButtonPause.Visibility = Visibility.Visible;
-            ButtonPrevious.IsEnabled = true;
-            ButtonNext.IsEnabled = true;
-            if (timer == null) StartNewRound(); else StartRound();
+            if (round == worker.Length - 1) round = 0; else round++;
+        }
+
+        private void PrevRound()
+        {
+            if (round == 0) round = worker.Length - 1; else round--;
         }
 
         private void OnTick(object source, EventArgs e)
@@ -120,88 +153,18 @@ namespace Pomodoro
 
         private void OnWorkTimeEnd(object source, EventArgs e)
         {
-            inWorkRound = false;
-
-            time = TimeSpan.FromMinutes(rest[round]);
-            NextRound();
-            
-            TimerMinutes.Content = time.Minutes;
-            TimerSeconds.Content = time.ToString("ss");
+            NextRound();   
 
             var helper = new FlashWindowHelper(Application.Current);
-            // Flashes the window and taskbar 5 times and stays solid 
-            // colored until user focuses the main window
             helper.FlashApplicationWindow();
             System.Media.SystemSounds.Exclamation.Play();
 
             ((DispatcherTimer)source).Stop();
-
-        }
-
-        private void OnRestTimeEnd(object source, EventArgs e)
-        {
-            inWorkRound = true;
-
-            time = TimeSpan.FromMinutes(rest[round]);
-            NextRound();
-            
-            TimerMinutes.Content = time.Minutes;
-            TimerSeconds.Content = time.ToString("ss");
-
-            var helper = new FlashWindowHelper(Application.Current);
-            // Flashes the window and taskbar 5 times and stays solid 
-            // colored until user focuses the main window
-            helper.FlashApplicationWindow();
-            System.Media.SystemSounds.Exclamation.Play();
-
-            ((DispatcherTimer)source).Stop();
-
-
-        }
-
-        //UI Methods
-
-        private void Dragable_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
-
-        private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void ButtonClose_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void ButtonNext_Click(object sender, RoutedEventArgs e)
-        {
-            NextRound();
-            inWorkRound = !inWorkRound;
-            timer.Stop();
-            seconds.Stop();
+            timer = null;
             StartNewRound();
+            timer.Start();
         }
 
-        private void ButtonPrevious_Click(object sender, RoutedEventArgs e)
-        {
-            PrevRound();
-            inWorkRound = !inWorkRound;
-            timer.Stop();
-            seconds.Stop();
-            StartNewRound();
-        }
-
-        private void ButtonPause_Click(object sender, RoutedEventArgs e)
-        {
-            timer.Stop();
-            seconds.Stop();
-            ButtonPause.Visibility = Visibility.Hidden;
-            ButtonPlay.Visibility = Visibility.Visible;
-            ButtonNext.IsEnabled = false;
-            ButtonPrevious.IsEnabled = false;
-        }
+       
     }
 }
